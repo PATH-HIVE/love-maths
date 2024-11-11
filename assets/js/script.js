fbe49fe436fcdf0efd5a68912e30b1ee5d2a0fd8
PATH-HIVE <a.bocktor@gmail.com>
2024-11-11 18:40:33 +0000

    add event listnersfor document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,46 @@
-function runGame () {
+// Wait for the DOM to finish loading before running the game
+// Get the buttons elements and add event listners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    // The following line is JavaScript statement that
+    //  selects all <button> elements in the HTML document 
+    // and assigns them to the variable buttons
+    let buttons = document.getElementsByTagName("button");
+    // for (let i = 0; i < buttons.length; i++)
+    // Add an event listener to each button in a collection of
+    //  buttons for the click event 
+    // When the button is clicked, the code inside function() 
+    // { ... } will execute.
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert('You clicked ${gameType}');
+            }
+        })
+    }
+})
+
+
+function runGame() {
 
 }
 
-function checkAnswer (){
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer () {
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore (){
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer () {
+function incrementWrongAnswer() {
 
 }
 
@@ -23,9 +49,9 @@ function displayAdditionQeustion() {
 }
 
 function displaySubtractQeustion() {
-    
+
 }
 
 function displayMultiplyQeustion() {
-    
+
 }
